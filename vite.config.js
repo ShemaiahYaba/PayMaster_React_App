8b28cafe5fddbc94814f531a954db8003141b65d
ShemaiahYaba <67932039+ShemaiahYaba@users.noreply.github.com>
2024-10-17 22:22:58 +0100

    Fifth Commit
    Updated vite.config.js
    add "dist", the Default output directory for Vite

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
+    outDir: "dist", // Default output directory for Vite
     rollupOptions: {
       external: ["axios"],
     },
