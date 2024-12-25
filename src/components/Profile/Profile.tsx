9052a246497c53702cb91f2b48d35a984e63a91d
ShemaiahYaba <67932039+ShemaiahYaba@users.noreply.github.com>
2024-12-25 06:18:57 +0100

    Update Profile.tsx

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { CgLogOut } from "react-icons/cg";
 import PropTypes from "prop-types";
-import Header from "../../Header";
+import Header from "../../Header.tsx";
 import Img08 from "../../assets/profile.png";
 import Img09 from "../../assets/bookmark.png";
 import Img10 from "../../assets/pools.png";
